fb64a841ef0558904dd99232bbdddad02d3874a2
lslaosang <lovelaosang@gmail.com>
2026-09-17 13:05:01 +0700

    Update API URL to Render

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ function App() {
     // ==========================================
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://mobile-price-api1.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
